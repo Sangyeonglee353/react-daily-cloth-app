87496a1b7b7e66a3445e0f8b9ab1af4d064ceeb5
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-12-06 21:05:25 +0900

    Desgin: Add closet gallery form

--- src/pages/ClosetPage/index.jsx
+++ src/pages/ClosetPage/index.jsx
@@ -6,18 +6,38 @@ const ClosetPage = () => {
     <section name="closet" className="w-full min-h-[1000px] flex ">
       <div className="w-full h-full flex flex-col justify-center max-w-screen-lg px-4 mx-auto mt-[100px]">
         <div className="w-full max-h-[1000px] flex flex-col bg-blue-800">
-          <div className="w-full h-[80px] flex justify-around items-center space-x-auto bg-green-500">
+          <div className="w-full h-[80px] flex items-center bg-green-500">
             {/* 메뉴 Area */}
-            <ClosetButton />
-            <ClosetButton />
-            <ClosetButton />
-            <ClosetButton />
-            <ClosetButton />
-            <ClosetButton />
-            <ClosetButton />
+            <ul className="w-full flex justify-around">
+              <ClosetButton />
+              <ClosetButton />
+              <ClosetButton />
+              <ClosetButton />
+              <ClosetButton />
+              <ClosetButton />
+              <ClosetButton />
+            </ul>
           </div>
-          <div className="w-full h-full flex bg-orange-300">
+          <div className="w-full h-[1000px] flex bg-orange-300">
             {/* 옷 나열 리스트 Area */}
+            <ul className="w-full h-auto flex flex-wrap justify-around items-center">
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+              <div className="w-[200px] h-[200px] bg-gray-300 rounded-md"></div>
+            </ul>
           </div>
         </div>
       </div>
